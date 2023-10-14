d4cfb151ea8619ff473396cf9604c19f0afd97b9
Chris Paschall <ChrisP1108@gmail.com>
2023-10-14 16:36:53 -0700

    Links rendering to mobile preview initial completion

---
+++ frontend/scripts/profile/mobile_link_item.class.js
@@ -0,0 +1,70 @@
+import Profile from '/frontend/scripts/profile/profile.class.js';
+
+export default class MobileLinkItem {
+
+    // PROPERTIES
+
+    #data;
+    #node;
+    #linkArrowSVG = `<svg class="link-arrow" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.666626 5.3333V6.66664H8.66663L4.99996 10.3333L5.94663 11.28L11.2266 5.99997L5.94663 0.719971L4.99996 1.66664L8.66663 5.3333H0.666626Z" />
+        </svg>
+    `
+
+    // METHODS
+
+    // Render HTML
+
+    html() {
+        if (this.#linkDataFound()) {
+            this.#node.style.background = this.#data.color;
+            this.#node.innerHTML = `
+                <a class="mobile-click-link" ${this.#data.link ? `href="${this.#data.link}"` : ``} target="_blank" rel="nofollow">
+                    ${this.#data.iconSVG}
+                    <span class="mobile-link-text">${this.#data.label}</span>
+                    ${this.#linkArrowSVG}
+                </a>
+            `;
+        }
+    }
+
+    #linkDataFound() {
+        return this.#data !== null;
+    }
+
+    // CONSTRUCTOR
+
+    constructor(node) {
+
+        // Select Node
+
+        this.#node = node;
+
+        // Clear link node back to filler to ensure deleted links get cleared
+
+        this.#node.innerHTML = '';
+        this.#node.style.background = '';
+
+        // Get link data
+
+        const linkData = Profile.getData().links.find(link => link.order === Number(this.#node.dataset.order));
+
+        if (linkData) {
+
+            // Get option data corresponding to link data
+
+            const optionData = Profile.getPlatformDropdownOptions().find(option => option.value === linkData.platform);
+        
+            this.#data = {
+                value: optionData.value,
+                label: optionData.label,
+                color: optionData.color,
+                iconSVG: optionData.iconSVG,
+                link: linkData.link,
+                order: linkData.order,
+            }
+        } else {
+            this.#data = null;
+        }
+    }
+}
--- frontend/scripts/profile/mobile_preview_handler.class.js
+++ frontend/scripts/profile/mobile_preview_handler.class.js
@@ -1,45 +1,88 @@
 import Profile from '/frontend/scripts/profile/profile.class.js';
+import MobileLinkItem from '/frontend/scripts/profile/mobile_link_item.class.js';
 
 export default class MobilePreviewHandler {
 
+    static #linkItems;
+    static #blankMobileItems = 5;
+
     // METHODS
+
+    // Clear existing mobile link items to prevent errors when adding/removing links
+
+    static #clearExistingMobileItems() {
+
+        // Select All link node fillers
+        
+        MobilePreviewHandler.#linkItems = Profile.getNodes().mobileSection.linksSection.querySelectorAll(`[data-mobilelinkitem]`);
+    
+        MobilePreviewHandler.#linkItems.forEach((item, index) => {
+            item.innerHTML = '';
+            item.dataset.order = index + 1;
+            if (index + 1 > MobilePreviewHandler.#blankMobileItems) {
+                item.remove();
+            }
+        });
+    }
+
+    static #renderLinkItems() {
+
+        // Select All link node fillers
+        
+        MobilePreviewHandler.#linkItems = Profile.getNodes().mobileSection.linksSection.querySelectorAll(`[data-mobilelinkitem]`);
+        
+        // Assign link data to the 5 link node fillers if any link data found
+
+        MobilePreviewHandler.#linkItems.forEach(node => {
+
+            const mobileLink = new MobileLinkItem(node);
+
+            mobileLink.html();
+
+        });
+
+        // Create additional nodes if there are more than 5 links or more than blank mobile link items
+
+        if (Profile.getData().links.length > MobilePreviewHandler.#linkItems.length) {
+
+            // Iterate through remaining link data items greater than length of existing mobileLinkItems
+
+            Profile.getData().links.forEach((link, index) => {
+                if (index + 1 > MobilePreviewHandler.#linkItems.length) {
+
+                    // Clone blank mobile link node
+
+                    const nodeCopy = MobilePreviewHandler.#linkItems[0].cloneNode(true);
+
+                    // Adjust node dataset order number accordingly
+
+                    nodeCopy.dataset.order = index + 1;
+
+                    // Append node to links section
+
+                    Profile.getNodes().mobileSection.linksSection.appendChild(nodeCopy)
+
+                    // Generate mobile link items html
+
+                    const mobileLinkToRender = new MobileLinkItem(nodeCopy);
+
+                    mobileLinkToRender.html();
+                }
+            });
+        }
+    }
 
     // Render data to mobile preview 
 
     static renderMobilePreview() {
 
-        // Set links
+        // Clear existing mobile link items to prevent errors when adding/removing links
 
-        // Select All link node fillers
-        
-        const mobileLinkItems = Profile.getNodes().mobileSection.linksSection.querySelectorAll(`[data-mobilelinkitem]`);
+        MobilePreviewHandler.#clearExistingMobileItems();
 
-        // Assign link data to link node fillers if any link data found
+        // Render link items.  Fills in any blank gray mobile items fillers with linkn
 
-        mobileLinkItems.forEach(node => {
+        MobilePreviewHandler.#renderLinkItems();
 
-            // Clear link node back to filler to ensure deleted links get cleared
-
-            node.innerHTML = '';
-            node.style.background = '';
-
-            // Get link data
-
-            const linkData = Profile.getData().links.find(link => link.order === Number(node.dataset.order));
-
-            // If corresponding link data found, get option data and render.
-
-            if (linkData) {
-
-                // Get option data corresponding to link data
-
-                const optionData = Profile.getPlatformDropdownOptions().find(option => option.value === linkData.platform);
-            
-                if (linkData) {
-                    node.style.background = optionData.color;
-                    node.innerHTML = `<h4>${optionData.label}</h4>`;
-                }
-            }
-        });
     }
 }
