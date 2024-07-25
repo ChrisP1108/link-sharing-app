f71948db629d63fabc2344e2c67d1d443d0caab0
Chris Paschall <ChrisP1108@gmail.com>
2024-07-24 20:58:01 -0700

    Update get_user_data.php

--- frontend/utilities/get_user_data.php
+++ frontend/utilities/get_user_data.php
@@ -6,23 +6,24 @@
             $cookie = $_COOKIE[$_ENV['WEB_TOKEN_NAME']] ?? null;
             $id = Token::get_cookie_id($cookie);
         } else $id = $id_param;
-        
+
         if ($id !== null && $id !== '' && intval($id) !== 0) {
             $data = DATABASE->get_table_data(intval($id));
-            
+
             if ($data) {
                 $decode_links = html_entity_decode($data['links']);
                 $links_data = json_decode($decode_links, true);
-                unset($data['links']);         
-                foreach($links_data as $key => $link) {
-                    $links_data[$key]['platform'] = htmlspecialchars($link['platform']);
-                    $links_data[$key]['link'] = htmlspecialchars($link['link']);
-                }
+                unset($data['links']);
+                if($links_data) {
+                    foreach ($links_data as $key => $link) {
+                        $links_data[$key]['platform'] = htmlspecialchars($link['platform']);
+                        $links_data[$key]['link'] = htmlspecialchars($link['link']);
+                    }
+                } else $links_data = [];
                 $parsed_data = Controller_Utilities::parse_user_keys($data);
                 $output_data = Sanitize::sanitize_data($parsed_data);
                 $output_data['links'] = $links_data;
                 return json_encode($output_data);
-            
             } else return json_encode(false);
         } else return json_encode(false);
     }
